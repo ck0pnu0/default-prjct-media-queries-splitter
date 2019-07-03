4cda697a2b2deb27583e7f2b7af00f265c2dd4b3
ck0pnu0 <georgi.dimitrov89@gmail.com>
2019-07-03 22:57:28 +0300

    fix gulp async issues (v. 4) new css files were created

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ function errorlog(err){
 // Scripts Tasks
 // ///////////////////////////////////////////////
 
-gulp.task('scripts', function() {
+gulp.task('scripts', gulp.series(function() {
   return gulp.src(config.jsConcatFiles)
 	.pipe(sourcemaps.init())
 		.pipe(concat('functions.js'))
@@ -63,14 +63,14 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./app/js/'))
 
     .pipe(reload({stream:true}));
-});
+}));
 
 
 // ////////////////////////////////////////////////
 // Styles Tasks
 // ///////////////////////////////////////////////
 
-gulp.task('split-css', function () {
+gulp.task('sass', gulp.series( function () {
 	// Sass plugins
 	var sass_plugins = [
 		// autoprefixer({ browsers: ['last 3 version'] })
@@ -89,14 +89,24 @@ gulp.task('split-css', function () {
 	};
 
 	var pipeErrorHandler = plumber(errorHandler);
-	
-	gulp.src('app/scss/style.scss')
+
+	var task = gulp.src('app/scss/style.scss')
 		.pipe(pipeErrorHandler) 							// Prevent pipe breaking caused by errors from gulp plugins
 		.pipe(sourcemaps.init())							// source map init
 		.pipe(sass( {
 			plugins: sass_plugins
 		}).on('error', errorHandler))						// Sass
 		.pipe(sourcemaps.write( '.' ))						// sourcemap write
+		.pipe(gulp.dest( 'app/css' )) 							// save css file
+		.pipe(filter('**/*.css')) 							// filter only css files (remove the map file)
+		.pipe(reload({stream: true})); 						// inject the changed css
+
+
+	return task;
+}));
+
+gulp.task('split-css', gulp.series(function (done) {
+	gulp.src('app/css/style.css')
         .pipe(mediaQueriesSplitter([						// media Queries Splitter start from here
             // Include all CSS rules
             {media: 'all', filename: 'all.css'},
@@ -119,80 +129,41 @@ gulp.task('split-css', function () {
             // Include CSS rules for bigger screen sizes (mostly used on desktop)
             {media: {min: '1280px'}, filename: 'desktop-large.css'},
         ]))
-		.pipe(gulp.dest('./app/css'))
-		.pipe(filter('**/*.css'))
-		.pipe(reload({stream: true}));
-});
-
-/**
- * Comment out the following loading of the sass files for testing purposes
- */
-// gulp.task('sass', function () {
-	// // Sass plugins
-	// var sass_plugins = [
-	// 	// autoprefixer({ browsers: ['last 3 version'] })
-	// ];
-
-	// // Sass task error handler
-	// var errorHandler = function(errorObj) {
-	// 	// Notify the user
-	// 	browserSync.notify('Error: ' + beautifyMessage(errorObj.message));
-
-	// 	// Post the message in the console
-	// 	console.log(errorObj.message);
-
-	// 	// End this task
-	// 	this.emit('end');
-	// };
-
-	// var pipeErrorHandler = plumber(errorHandler);
-
-// 	var task = gulp.src('app/scss/style.scss')
-		// .pipe(pipeErrorHandler) 							// Prevent pipe breaking caused by errors from gulp plugins
-		// .pipe(sourcemaps.init())							// source map init
-		// .pipe(sass( {
-		// 	plugins: sass_plugins
-		// }).on('error', errorHandler))						// Sass
-		// .pipe(sourcemaps.write( '.' ))						// sourcemap write
-// 		.pipe(gulp.dest( 'app/css' )) 							// save css file
-// 		.pipe(filter('**/*.css')) 							// filter only css files (remove the map file)
-// 		.pipe(reload({stream: true})); 						// inject the changed css
-
-
-// 	return task;
-// });
-
+		.pipe(gulp.dest('./app/css'));
+		done();
+}));
 
 // ////////////////////////////////////////////////
 // HTML Tasks
 // // /////////////////////////////////////////////
 
-gulp.task('html', function(){
+gulp.task('html', gulp.series(function(done){
     gulp.src('app/**/*.html')
-    .pipe(reload({stream:true}));
-});
+	.pipe(reload({stream:true}));
+	done();
+}));
 
 
 // ////////////////////////////////////////////////
 // Browser-Sync Tasks
 // // /////////////////////////////////////////////
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', gulp.series(function() {
     browserSync({
         server: {
             baseDir: "./app/"
         }
     });
-});
+}));
 
 // task to run build server for testing final app
-gulp.task('build:serve', function() {
+gulp.task('build:serve', gulp.series(function() {
     browserSync({
         server: {
             baseDir: "./build/"
         }
     });
-});
+}));
 
 
 // ////////////////////////////////////////////////
@@ -200,40 +171,39 @@ gulp.task('build:serve', function() {
 // // /////////////////////////////////////////////
 
 // clean out all files and folders from build folder
-gulp.task('build:cleanfolder', function (cb) {
+gulp.task('build:cleanfolder', gulp.series(function (cb) {
 	del([
 		'build/**'
 	], cb);
-});
+}));
 
 // task to create build directory of all files
-gulp.task('build:copy', ['build:cleanfolder'], function(){
+gulp.task('build:copy', gulp.series('build:cleanfolder', function(){
     return gulp.src('app/**/*/')
     .pipe(gulp.dest('build/'));
-});
+}));
 
 // task to removed unwanted build files
 // list all files and directories here that you don't want included
-gulp.task('build:remove', ['build:copy'], function (cb) {
+gulp.task('build:remove', gulp.series('build:copy', function (cb) {
 	del(config.buildFilesFoldersRemove, cb);
-});
+}));
 
-gulp.task('build', ['build:copy', 'build:remove']);
+gulp.task('build', gulp.series('build:copy', 'build:remove'));
 
 
 // ////////////////////////////////////////////////
 // Watch Tasks
 // // /////////////////////////////////////////////
 
-gulp.task ('watch', function(){
+gulp.task('watch', gulp.series(function(){
 	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/css/*.css', ['split-css']);
 	gulp.watch('app/js/**/*.js', ['scripts']);
   	gulp.watch('app/**/*.html', ['html']);
-});
+}));
 
-
-gulp.task('default', ['scripts', 'sass', 'html', 'browser-sync', 'watch']);
-
+gulp.task('default', gulp.series('scripts', 'sass', 'split-css', 'html', 'browser-sync', 'watch'));
 
 // Helpers
 
